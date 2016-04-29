dda83a9a1c39866b28436647e94c7f7381b0dd54
RamEternus <ram.subbaiyan@eternussolutions.com>
2016-04-29 19:43:11 +0530

    Estimation
    Estimation Guidelines

--- src/controller/projectcontroller.py
+++ src/controller/projectcontroller.py
@@ -6,6 +6,7 @@ import os.path
 import webapp2
 import time
 from model import user
+from model import project
 import webapp2_extras.appengine.auth.models as auth_user
 from webapp2_extras import auth
 from webapp2_extras import sessions
@@ -20,7 +21,7 @@ from datetime import datetime
 class ProjectManagement(BaseHandler):
     def get(self):
         if check_permission(self):
-            projmodel=user.Project()
+            projmodel=project.Project()
             proj=projmodel.get_all()
             usermodel = user.OurUser().get_all()
             self.render_template("project-management-new.html",{"project":proj,"userslist":usermodel,'permission':'success'})
@@ -28,18 +29,58 @@ class ProjectManagement(BaseHandler):
         else:
             #self.response.write("you are not allowed")
             self.render_template("project-management-new.html",{'not_permitted':'You are not authorized to view this page'})    
+
+
+
 class AddProject(BaseHandler):
         
     
     def post(self):
         logging.info("it is here "+self.request.__str__())
-        project=user.Project()
-        project.name = self.request.get("proj_name")
-        project.description = self.request.get("proj_desc")
-        project.startDate = datetime.strptime(self.request.get("proj_start"), '%d/%m/%Y').date()
-        project.endDate = datetime.strptime(self.request.get("proj_end"), '%d/%m/%Y').date()
-        project.team = (self.request.get("proj_team")).split(",")
-        project.set()
+        projec=project.Project()
+        projec.name = self.request.get("proj_name")
+        projec.description = self.request.get("proj_desc")
+        projec.startDate = datetime.strptime(self.request.get("proj_start"), '%d/%m/%Y').date()
+        projec.endDate = datetime.strptime(self.request.get("proj_end"), '%d/%m/%Y').date()
+        projec.team = (self.request.get("proj_team")).split(",")
+        projkey = projec.set()
+        #logging.info(projkey)
+        
+        estimation = project.Estimation()
+        estimation.projectid=projkey
+        estimation.estimationLevel="Very Simple"
+        estimation.estimationPoint=int(self.request.get("VsPoint"))
+        estimation.estimationHours=float(self.request.get("VsHour"))
+        estimation.set()
+        
+        estimation = project.Estimation()
+        estimation.projectid=projkey
+        estimation.estimationLevel="Simple"
+        estimation.estimationPoint=int(self.request.get("SPoint"))
+        estimation.estimationHours=float(self.request.get("SHour"))
+        estimation.set()
+        
+        estimation = project.Estimation()
+        estimation.projectid=projkey
+        estimation.estimationLevel ="Medium"
+        estimation.estimationPoint=int(self.request.get("MPoint"))
+        estimation.estimationHours=float(self.request.get("MHour"))
+        estimation.set()
+        
+        estimation = project.Estimation()
+        estimation.projectid=projkey
+        estimation.estimationLevel="Complex"
+        estimation.estimationPoint=int(self.request.get("CPoint"))
+        estimation.estimationHours=float(self.request.get("CHour"))
+        estimation.set()
+        
+        estimation = project.Estimation()
+        estimation.projectid=projkey
+        estimation.estimationLevel="Very Complex"
+        estimation.estimationPoint=int(self.request.get("VcPoint"))
+        estimation.estimationHours=float(self.request.get("VcHour"))
+        estimation.set()
+        
         self.response.write("true")
         
 class GetTeamMembersForProject(BaseHandler):
@@ -49,3 +90,19 @@ class GetTeamMembersForProject(BaseHandler):
         project = projectkey.get()
         
         self.render_template("viewteammembers.html",{"project":project})
+        
+class EditProject(BaseHandler):
+    def get(self):
+       
+      
+        projectkey = ndb.Key(urlsafe=self.request.get("key"))
+        project_list = projectkey.get()
+        
+   
+        
+        estimation = project.Estimation()
+        esti = estimation.query(project.Estimation.projectid == projectkey ).fetch()
+        
+        logging.info(esti)
+        
+        self.render_template("editproject.html",{"project":project_list,"estimation":esti})
--- src/model/project.py
+++ src/model/project.py
@@ -9,6 +9,19 @@ class Project(ndb.Model):
     startDate = ndb.DateProperty()
     endDate = ndb.DateProperty()
     team = ndb.StringProperty(repeated=True)
+    companyid = ndb.KeyProperty(repeat=True)
+    
+    def set(self):
+        return self.put()
+    def get_all(self):
+        res = self.query().fetch()
+        return res
+    
+class Estimation(ndb.Model):
+    projectid =  ndb.KeyProperty(required=True)
+    estimationLevel = ndb.StringProperty(required=True)
+    estimationPoint = ndb.IntegerProperty(required=True)
+    estimationHours = ndb.FloatProperty(required=True)
     
     def set(self):
         self.put()
