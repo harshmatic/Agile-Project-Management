54907686adc546ebb47ae81f97e3004909642062
RamEternus <ram.subbaiyan@eternussolutions.com>
2016-05-10 15:41:23 +0530

    Project_New Design
    Project New Design

--- src/controller/projectcontroller.py
+++ src/controller/projectcontroller.py
@@ -17,8 +17,25 @@ from webapp2_extras import security
 from login import BaseHandler,check_permission
 from google.appengine.ext import ndb
 from datetime import datetime
+import json
 
 class ProjectManagement(BaseHandler):
+    def get(self,*args,**kargs):
+        if check_permission(self):
+            projmodel=project.Project()
+            proj=projmodel.get_all()
+            currentUser=self.auth.get_user_by_session()
+            usermodel = user.OurUser().query(user.OurUser.tenant_key==self.user_model.get_by_id(currentUser['user_id']).tenant_key)
+            self.render_template("user_new/apm-all-projects.html",{"project":proj})
+            #self.render_template("user_new/apm-add-project.html")
+           
+        else:
+            #self.response.write("you are not allowed")
+            #self.render_template("apm-add-project.html",{'not_permitted':'You are not authorized to view this page'})    
+            self.render_template("user_new/apm-all-projects.html")
+
+
+class AddProjectView(BaseHandler):
     def get(self,*args,**kargs):
         if check_permission(self):
             projmodel=project.Project()
@@ -31,10 +48,7 @@ class ProjectManagement(BaseHandler):
         else:
             #self.response.write("you are not allowed")
             #self.render_template("apm-add-project.html",{'not_permitted':'You are not authorized to view this page'})    
-            self.render_template("user_new/apm-add-project.html")
-
-
-        
+            self.render_template("user_new/apm-add-project.html")       
         
 class AddProject(BaseHandler):
         
@@ -56,42 +70,210 @@ class AddProject(BaseHandler):
         projkey = projec.set()
         #logging.info(projkey)
         estimation = project.Estimation()
-        estimation.projectid=projkey
+        estimation.projectid=projkey.id()
+        estimation.companyid = companyId.id()
         estimation.estimationLevel="Very Simple"
         estimation.estimationPoint= 1
         estimation.estimationHours= 1.0
         estimation.set()
         
         estimation = project.Estimation()
-        estimation.projectid=projkey
+        estimation.projectid=projkey.id()
+        estimation.companyid = companyId.id()
         estimation.estimationLevel="Simple"
         estimation.estimationPoint= 1
         estimation.estimationHours= 1.0
         estimation.set()
         
         estimation = project.Estimation()
-        estimation.projectid=projkey
+        estimation.projectid=projkey.id()
+        estimation.companyid = companyId.id()
         estimation.estimationLevel ="Medium"
         estimation.estimationPoint= 1
         estimation.estimationHours= 1.0
         estimation.set()
         
         estimation = project.Estimation()
-        estimation.projectid=projkey
+        estimation.projectid=projkey.id()
+        estimation.companyid = companyId.id()
         estimation.estimationLevel="Complex"
         estimation.estimationPoint= 1
         estimation.estimationHours= 1.0
         estimation.set()
         
         estimation = project.Estimation()
-        estimation.projectid=projkey
+        estimation.projectid=projkey.id()
+        estimation.companyid = companyId.id()
         estimation.estimationLevel="Very Complex"
         estimation.estimationPoint= 1
         estimation.estimationHours= 1.0
         estimation.set()
         
+        projemem = project.ProjectMembers()
+        projemem.userName =  self.user_model.get_by_id(currentUser['user_id']).name
+        projemem.projectid = projkey.id()
+        projemem.companyid = companyId.id()
+        projemem.userid =   currentUser['user_id']
+        projemem.userRole = "Product Owner"
+        projemem.set()
         
-        self.response.write("true")
+        self.response.write(projkey.id())
+        
+class EditProj(BaseHandler):
+        
+    
+    def post(self,*args,**kargs):
+        logging.info("it is here "+self.request.__str__())
+        projkey = ndb.Key('Project',int(self.request.get("projid")))
+        projec =  projkey.get()
+        projec.name = self.request.get("proj_name")
+        projec.description = self.request.get("proj_desc")
+        projec.startDate = datetime.strptime(self.request.get("proj_start"), '%d/%m/%Y').date()
+        projec.endDate = datetime.strptime(self.request.get("proj_end"), '%d/%m/%Y').date()
+        #projec.team = (self.request.get("proj_team")).split(",")
+        projkey = projec.set()
+        
+        self.response.write(projkey.id())
+                
+class AddProjectMembers(BaseHandler):
+    def post(self,*args,**kargs):
+        currentUser=self.auth.get_user_by_session()
+        logging.info("it is here "+self.request.__str__())
+        userid = self.request.get("userid")
+        projid = self.request.get("projid")
+        role   = self.request.get("role")
+        logging.info("it is here and the userId is"+userid)
+        logging.info("it is here and the projid is"+projid)
+        logging.info("it is here and the role is"+role)
+        
+        companyId= self.user_model.get_by_id(currentUser['user_id']).tenant_key
+        
+        projemem = project.ProjectMembers()
+        userkey = ndb.Key('OurUser',int(userid))
+        model = userkey.get()
+        projemem.userName =  model.name
+        projemem.projectid = int(projid)
+        projemem.companyid = companyId.id()
+        projemem.userid =    int(userid)
+        projemem.userRole = role
+        projekey = projemem.set()
+        projmodel = projekey.get()
+        
+        data = {}
+        data['id'] = projmodel.key.id()
+        data['projectid'] = projmodel.projectid
+        data['companyid'] = projmodel.companyid
+        data['userName'] = projmodel.userName
+        data['userid'] = projmodel.userid
+        data['userRole'] = projmodel.userRole
+        
+        self.response.write(json.dumps(data, ensure_ascii=False))
+
+class EditProjMem(BaseHandler):
+        
+    
+    def post(self,*args,**kargs):
+        logging.info("it is here "+self.request.__str__())
+        projkey = ndb.Key('ProjectMembers',int(self.request.get("userid")))
+        projemem =  projkey.get()
+        projemem.userRole = self.request.get("role")
+        #projec.team = (self.request.get("proj_team")).split(",")
+        projmemkey = projemem.set()
+        projmemmodel = projmemkey.get()
+        data = {}
+        data['id'] = projmemmodel.key.id()
+        data['projectid'] = projmemmodel.projectid
+        data['companyid'] = projmemmodel.companyid
+        data['userName'] = projmemmodel.userName
+        data['userid'] = projmemmodel.userid
+        data['userRole'] = projmemmodel.userRole
+        
+        self.response.write(json.dumps(data, ensure_ascii=False))
+        
+        
+class AddProjectEstimates(BaseHandler):
+    def post(self,*args,**kargs):
+        currentUser=self.auth.get_user_by_session()
+        logging.info("it is here "+self.request.__str__())
+        complexity = self.request.get("complexity")
+        points = self.request.get("points")
+        efforts   = self.request.get("efforts")
+        projid = self.request.get("projid")
+        logging.info("it is here and the userId is"+complexity)
+        logging.info("it is here and the projid is"+points)
+        logging.info("it is here and the role is"+efforts)
+        
+        companyId= self.user_model.get_by_id(currentUser['user_id']).tenant_key
+        
+        estimation = project.Estimation()
+        estimation.projectid=int(projid)
+        estimation.companyid = companyId.id()
+        estimation.estimationLevel=  complexity
+        estimation.estimationPoint= int(points)
+        estimation.estimationHours= float(efforts)
+        estimateskey = estimation.set()
+        
+        estimatesmodel = estimateskey.get()
+        
+        data = {}
+        data['id'] = estimatesmodel.key.id()
+        data['projectid'] = estimatesmodel.projectid
+        data['companyid'] = estimatesmodel.companyid
+        data['estimationLevel'] = estimatesmodel.estimationLevel
+        data['estimationPoint'] = estimatesmodel.estimationPoint
+        data['estimationHours'] = estimatesmodel.estimationHours
+        
+        self.response.write(json.dumps(data, ensure_ascii=False))
+
+
+class DeleteProject(BaseHandler):
+    def post(self,*args,**kargs):
+        logging.info("it is here "+self.request.__str__())
+        projid = self.request.get("id")
+        proj = project.Project()
+        proj.delete_entity(projid)
+        self.response.write("success")
+        
+class DeleteProjectMember(BaseHandler):
+    def post(self,*args,**kargs):
+        logging.info("it is here "+self.request.__str__())
+        projmemid = self.request.get("id")
+        projmem = project.ProjectMembers()
+        projmem.delete_entity(projmemid)
+        self.response.write("success")
+        
+class DeleteEstimates(BaseHandler):
+    def post(self,*args,**kargs):
+        logging.info("it is here "+self.request.__str__())
+        estimatesid = self.request.get("id")
+        esti = project.Estimation()
+        esti.delete_entity(estimatesid)
+        self.response.write("success")
+        
+class ViewProject(BaseHandler):
+    def get(self,*args,**kargs):
+        currentUser=self.auth.get_user_by_session()
+        logging.info("it is here proj "+self.request.get("proj_id"))
+        projkey = int(self.request.get("proj_id"))
+        projmodel=project.Project()
+        proj=projmodel.get_proj(projkey)
+        
+        #proj.startDate = proj.startDate.strftime('%d/%m/%Y')
+        #proj.endDate =   proj.endDate.strftime('%d/%m/%Y')
+        logging.info("it is here proj "+proj.__str__())
+        
+        estimationmodel = project.Estimation()
+        esti = estimationmodel.get_all(projkey)
+        
+        projectmembermodel = project.ProjectMembers()
+        projmem = projectmembermodel.get_all(projkey)
+        
+        groupmodel = user.Groups().query(user.Groups.tenant_key==self.user_model.get_by_id(currentUser['user_id']).tenant_key)
+        
+        currentUser=self.auth.get_user_by_session()
+        usermodel = user.OurUser().query(user.OurUser.tenant_key==self.user_model.get_by_id(currentUser['user_id']).tenant_key)
+        self.render_template("user_new/viewproject.html",{"project":proj,"userslist":usermodel,'estimation':esti,'projmem':projmem,'roles':groupmodel})
+        
         
 class GetTeamMembersForProject(BaseHandler):
     def get(self,*args,**kargs):
--- src/model/project.py
+++ src/model/project.py
@@ -16,15 +16,39 @@ class Project(ndb.Model):
     def get_all(self):
         res = self.query().fetch()
         return res
+    def get_proj(self,projid):
+        projKey = ndb.Key('Project',projid)
+        res = projKey.get()
+        return res
+    def delete_entity(self,id):
+        ndb.Key(Project, int(id)).delete()
     
 class Estimation(ndb.Model):
-    projectid =  ndb.KeyProperty(required=True)
+    projectid =        ndb.IntegerProperty(required=True)
+    companyid =       ndb.IntegerProperty(required=True)
     estimationLevel = ndb.StringProperty(required=True)
     estimationPoint = ndb.IntegerProperty(required=True)
     estimationHours = ndb.FloatProperty(required=True)
     
     def set(self):
-        self.put()
-    def get_all(self):
-        res = self.query().fetch()
+        return self.put()
+    def get_all(self,projId):
+        res = self.query(Estimation.projectid == projId).fetch()
         return res
+    def delete_entity(self,id):
+        ndb.Key(Estimation, int(id)).delete()
+    
+class ProjectMembers(ndb.Model):
+    projectid =        ndb.IntegerProperty(required=True)
+    companyid =       ndb.IntegerProperty(required=True)
+    userName = ndb.StringProperty(required=True)
+    userid   = ndb.IntegerProperty(required=True)
+    userRole = ndb.StringProperty(required=True)
+    
+    def set(self):
+        return self.put()
+    def get_all(self,projId):
+        res = self.query(ProjectMembers.projectid == projId).fetch()
+        return res
+    def delete_entity(self,id):
+        ndb.Key(ProjectMembers, int(id)).delete()
