aeea3ccc22f383e06487c82224f7b6b81becc965
harshmatic <harshit.jyoti@eternussolutions.com>
2016-05-11 12:53:35 +0530

    admin

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from webapp2_extras import routes
 from webapp2_extras.routes import DomainRoute
 
 app = webapp2.WSGIApplication([
-    DomainRoute('<subdomain>.<version>.apm-eternus.appspot.com', [
+    DomainRoute('<subdomain>.apm-eternus.appspot.com', [
         webapp2.Route('/', Main, name='subdomain-home'),
         webapp2.Route('/dashboard', EndUserDashboardHandler, name='dashboard'),                           
         webapp2.Route('/admin/signup', SignupHandler, name='adminsignup'),
