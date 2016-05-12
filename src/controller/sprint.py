a9d4e8bc40249b7aa9607129de2f33874e7c8263
harshmatic <harshit.jyoti@eternussolutions.com>
2016-05-12 14:27:50 +0530

    Sprint

--- src/controller/sprint.py
+++ src/controller/sprint.py
@@ -4,14 +4,14 @@ from model import user
 from login import BaseHandler,check_permission
 #import simplejson as json
 import json as json
-from model import task
+from model import sprint
 
 from google.appengine.api import mail
 from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.ext import blobstore
 import webapp2
 
-class Sprint(BaseHandler):
+class Tasks(BaseHandler):
     
     def get(self,*args,**kargs):
         #if check_permission(self):
@@ -30,3 +30,28 @@ class Sprint(BaseHandler):
             self.response.write("true"+password)
         else:
             self.response.write("User is already verified.")
+            
+class Sprint(BaseHandler):
+    
+    def get(self,*args,**kargs):
+        #if check_permission(self):
+            sprint_data=sprint.Sprint().get_all()
+            self.render_template("user_new/apm-sprint-items.html",{"type":sprint_data})
+        #else:
+            #self.response.write("you are not allowed")
+    
+    def post(self,*args,**kargs):
+        currentUser=self.auth.get_user_by_session()
+        createdBy=self.user_model.get_by_id(currentUser['user_id']).key
+        sprint=sprint.Sprint()
+        sprint.name = self.request.get("name")
+        sprint.description = self.request.get("desc")
+        sprint.startDate = self.request.get("start")
+        sprint.endDate = self.request.get("end")
+        sprint.project = self.request.get("project_key")
+        sprint.createdby = createdBy
+        sprint.status = "Open"
+        sprint.set()
+        self.response.out.write("true")
+        
+        
---
+++ src/model/sprint.py
@@ -0,0 +1,19 @@
+from google.appengine.ext import ndb
+
+from model.user import *
+
+class Sprint(ndb.Model):
+    name = ndb.StringProperty(required=True)
+    description = ndb.StringProperty()
+    createdDate = ndb.DateTimeProperty(auto_now_add=True)
+    startDate = ndb.DateProperty()
+    endDate = ndb.DateProperty()
+    project = ndb.KeyProperty()
+    createdby = ndb.KeyProperty()
+    status = ndb.StringProperty()
+    def set(self,data):
+        self.put()
+    def get_all(self):
+        return self.query().fetch()  
+    
+    
