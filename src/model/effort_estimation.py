06b397b3346eb7e194d47ce6849f7d02d3c27cf7
RamEternus <ram.subbaiyan@eternussolutions.com>
2016-05-23 18:54:08 +0530

    Effort_Estimation Model
    Model for Effort estimation.

---
+++ src/model/effort_estimation.py
@@ -0,0 +1,28 @@
+from google.appengine.ext import ndb
+import logging
+from model.user import *
+from base import BaseClass
+
+
+class UserEffort(ndb.Model):
+    userKey = ndb.KeyProperty()
+    userName = ndb.StringProperty()
+    effortHours = ndb.StringProperty()
+
+class EffortEstimation(BaseClass):
+    project = ndb.KeyProperty(required=True)
+    sprint = ndb.KeyProperty(required=True)
+    effort = ndb.StructuredProperty(UserEffort, repeated=True)
+    date =  ndb.DateProperty(required=True)
+    total_effort = ndb.StringProperty()
+    
+    def set(self):
+        return self.put()
+    
+    def get_all(self):
+        res = self.query().fetch()
+        return res
+    
+    def get_esti_by_sprint(self,sprint_id):
+        res = self.query(EffortEstimation.sprint==sprint_id).fetch()
+        return res
