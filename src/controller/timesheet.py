65a94a972019d58342dc1f064851bed401d6dd19
harshmatic <harshit.jyoti@eternussolutions.com>
2016-05-24 21:13:43 +0530

    Timesheet

--- src/controller/timesheet.py
+++ src/controller/timesheet.py
@@ -4,7 +4,7 @@ from model import user,project
 from login import BaseHandler,check_permission
 import json as json
 from model import sprint,task,time_log
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta,date
 import time
 class Timesheet(BaseHandler):
     
@@ -15,23 +15,25 @@ class Timesheet(BaseHandler):
         
         # day = time.strftime("%d/%m/%Y")
         #dt = datetime.strptime(day, '%d/%m/%Y')
-        dt=datetime.now()
+        dt=date.today()
         week=[];
         start = dt - timedelta(days=dt.weekday())
         for i in range(0,7):
             end = start + timedelta(days=i)
-            week.append(end.strftime("%d/%m/%Y"))
+            week.append(end)
         log_time={}
         end = start + timedelta(days=6)
-        print(start.strftime('%d/%m/%Y'))
-        print(end.strftime('%d/%m/%Y'))
         logs=time_log.Time_Log().getByProjectUser(projectKey,currentUser)
         for name in logs:
-            if name.today_date.strftime('%d/%m/%Y') in week:
+            if name.today_date in week:
                 key = name.today_date
                 if key not in log_time:
                     log_time[key] = []
+                    log_time[key].append(name)
+                else:
+                    log_time[key].append(name)
         for w in week:
-            if not log_time[w]:
+            if w not in log_time:
                 log_time[w] = []
-        self.render_template("user_new/timesheet.html",{"logs":log_time})
+        log_times=sorted(log_time.items())
+        self.render_template("user_new/timesheet.html",{"logs":log_times})
