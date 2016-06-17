c43d005fa45d4e7167ee2c921e717ae6379c15e4
Atul Sharma <aulsharma35@gmail.com>
2016-06-17 17:41:06 +0530

    Bug fixes

--- src/const.py
+++ src/const.py
@@ -4,5 +4,5 @@ Created on Jun 13, 2016
 @author: Atul Sharma
 '''
 
-APP_DOMAIN = 'kajal3.apm-eternus.appspot.com'
-OUT_MAIL_ADDRESS = 'support@appspotmail.com'
+APP_DOMAIN = 'ner-monty.appspot.com'
+OUT_MAIL_ADDRESS = 'support@ner-monty.appspotmail.com'
